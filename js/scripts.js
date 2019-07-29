254b79d0ce46d581b4b5247324eb96461d16d9cb
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-29 16:36:07 +0200

    added a readme file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,105 @@
-$(document).ready(function() {
-    $("#design").hide();
-    $("#development").hide();
-    $("#product").hide();
-    $("#despar").hide();
-    $("#devpar").hide();
-    $("#propar").hide();
-
-    $("#ign.label").click(function() {
-        $("#ign").hide();
-        $("label").hide();
-        $("#design").show();
-        $("#despar").show();
+$(document).ready(function(){
+    $("#des").click(function(){
+        $("img#ign").toggle();
+        $("p#ign").toggle();
+        $("p#despar").toggle();
     });
 
-    $("despar").click(function() {
-        $("#design").hide();
-        $("#despar").hide();
-        $("#ign").show();
-        $("#label").show();
+    $("#dev").click(function(){
+        $("img#ment").toggle();
+        $("p#ment").toggle();
+        $("p#devpar").toggle();
     });
 
-    $("#dev").click(function() {
-        $("#dev").hide();
-        $("#label").hide();
-        $("#development").show();
-        $("#devpar").show();
+    $("#pro").click(function(){
+        $("img#duct").toggle();
+        $("p#duct").toggle();
+        $("p#propar").toggle();
     });
-    $("#devpar").click(function() {
-        $("#devpar").hide();
-        $("#development").hide();
-        $("#dev").show();
-        $("#label").show();
-    });
-    $("#")
 
-}
+    $("p#one").hide();
+    $("#work1").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("p#one").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("p#one").fadeOut();
+    });
+
+    $("#two").hide();
+    $("#work2").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#two").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#two").fadeOut();
+    });
+
+    $("#three").hide();
+    $("#work3").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#three").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#three").fadeOut();
+    });
+
+    $("#four").hide();
+    $("#work4").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#four").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#four").fadeOut();
+    });
+
+    $("#five").hide();
+    $("#work5").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#five").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#five").fadeOut();
+    });
+
+    $("#six").hide();
+    $("#work6").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#six").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#six").fadeOut();
+    });
+
+    $("#seven").hide();
+    $("#work7").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#seven").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#seven").fadeOut();
+    });
+
+    $("#eight").hide();
+    $("#work8").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $("#eight").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $("#eight").fadeOut();
+    });
+
+    $("#submit").click(function(){
+        var name=$("#name").val();
+        var email=$("#email").val();
+        var message=$("#exampleFormControlTextArea").val();
+        if((name==="")||(email==="")||(message==="")){
+            alert("Please, fill in the empty boxes");
+        }
+        else{
+            alert(name+ ", we have received your message. Thank you for reaching out to us!");
+        }
+        event.preventDefault();
+    });
+      
+});
---
+++ readme.md
@@ -0,0 +1,26 @@
+## Delani Studio
+##Author:Eve Ishimwe
+This Delani Studio website is a week 3 assignment that will help us to use all the concepts we've studied so far.
+## Description
+Delani Studio Website is composed of profile pictures,an "about us" which describes what the project is going to do. A part of services which contains services available,a portfolio segment of different pictures up to eight that has their own story but related and has been done by Delani Studio,afterall there is a contact us 
+region where the user adds his/her name and email and a message about Delani Studio. At the end, we have social media websites of our Studio you can visit in case you want some information,new contents and a bit of what we've been doing so far which can also be used for booking in case you need our services.
+## Installation Requirements
+None
+## Behavior Drive Development
+1.The user will have to meet a profile picture of Delani Studio at first.
+2.The user will meet an about us for more information.
+3.The user has to see if the services he/she wants are delivered by this Studio in Services section.
+4.There is a segment of what we've done,you can compare to.
+5.For any comment,booking or any kind of message to our Studio,please fill the contact us page.
+## Bugs:
+None
+## Technologies Used:
+Html
+CSS
+Scripts
+JQuery
+## Contact Details:
+Email:eveish@gmail.com
+Phone number: 0788123450
+## License
+Copyright (c) 2019 Evelyne N. Ishimwe
